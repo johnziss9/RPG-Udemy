2a203b4aa4d063e63d336a9fc815c2f4cf9466e2
John Zissimou <ZissimouJ@b2b.regn.net>
2020-09-24 14:59:36 +0100

    Added a method for adding a new character.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -25,5 +25,12 @@ namespace dotnet_rpg_udemy.Controllers
         {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
     }
 }
