4d8ce4320cfa92e61d88b5465731f34df1182b7c
John Zissimou <ZissimouJ@b2b.regn.net>
2020-09-24 14:51:27 +0100

    Added an id parameter to the GetSingle method so we can request a specific id of a character.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using dotnet_rpg_udemy.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,11 +9,21 @@ namespace dotnet_rpg_udemy.Controllers
     [Route("[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Id = 1, Name = "Sam" }
+        };
 
+        [HttpGet("GetAll")]
         public IActionResult Get()
         {
-            return Ok(knight);
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
     }
 }
