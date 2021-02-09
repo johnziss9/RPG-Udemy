9a348d88daeac1d85a811d43098a22cd8d41da2e
John Z <johnziss9@gmail.com>
2021-02-09 10:47:48 +0000

    Created a VerifyPasswordHash which checks if the password matches. In the login method I've added a check if user exists and password is correct. Created a new Dto and called the method in the controller.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -27,5 +27,16 @@ namespace RPG_Udemy.Controllers
 
             return Ok(response);
         }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginDto request)
+        {
+            ServiceResponse<string> response = await _authRepo.Login(request.Username, request.Password);
+
+            if (!response.Success)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
     }
 }
---
+++ Dtos/User/UserLoginDto.cs
@@ -0,0 +1,8 @@
+namespace RPG_Udemy.Dtos.User
+{
+    public class UserLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
